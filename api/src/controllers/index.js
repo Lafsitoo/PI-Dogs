984f16134a5a7c86870714387c1683ca53668477
Lafsitoo <fretteleandro@live.com>
2023-01-03 21:58:13 -0300

    03/01-21:58

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const { API_KEY } = process.env;
 const API_ALL_URL = "https://api.thedogapi.com/v1/breeds";
 // Traigo los modelos
 const { Dog, Temperament } = require("../models");
