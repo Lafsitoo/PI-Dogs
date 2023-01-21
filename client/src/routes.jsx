7a69bffca73bb543dd3d33db75e6f3f2d30fb2a5
Lafsitoo <fretteleandro@live.com>
2023-01-21 20:10:09 -0300

    21/01-20:10

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "./pages/Landing/Landing";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={X} />
+        <Route index element={ <Landing /> } />
         <Route path={"/"} element={X} />
         <Route path={"/"} element={X} />
         <Route path={"/"} element={X} />
