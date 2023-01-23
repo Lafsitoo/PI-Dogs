4ccf4c56e5b0c8c85a5a069e508dd65b10a63c75
Lafsitoo <fretteleandro@live.com>
2023-01-22 21:37:07 -0300

    22/01-21:37

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+
+import { Link } from 'react-router-dom';
+import "./NavBar.css";
+
+function NavBar() {
+    return(
+        <header>
+            <nav>
+            <div>
+                <Link to="/">
+                    <button></button>
+                </Link>
+            </div>
+            <div>
+                <Link to="/dogs">
+                    <button></button>
+                </Link>
+            </div>
+            <div>
+                <Link to="/about">
+                    <button></button>
+                </Link>
+            </div>
+            </nav>
+        </header>
+    )
+}
+
+export { NavBar };
