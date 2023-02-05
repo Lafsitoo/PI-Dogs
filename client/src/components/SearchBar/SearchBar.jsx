dcb52e9a06c7eba5274ed31880a434d021c209d9
Lafsitoo <fretteleandro@live.com>
2023-02-04 22:27:08 -0300

    04/02-22:27

---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  const [input, setInput] = useState("");
+
+  return (
+    <form>
+      <input></input>
+    </form>
+  );
+};
+
+export { SearchBar };
