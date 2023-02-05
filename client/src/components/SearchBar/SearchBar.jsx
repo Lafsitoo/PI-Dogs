44d8a7d2a21ba8bc08c3875021ad73872bfef9b3
Lafsitoo <fretteleandro@live.com>
2023-02-04 22:38:47 -0300

    04/01-22:38

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -4,9 +4,21 @@ import "./SearchBar.css";
 const SearchBar = () => {
   const [input, setInput] = useState("");
 
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setInput(e.target.value);
+  };
+
   return (
     <form>
-      <input></input>
+      <input
+        type="text"
+        value={input}
+        className="input-search"
+        placeholder="Buscar..."
+        onChange={(e) => handleInputChange(e)}
+      ></input>
+      <input type="submit"></input>
     </form>
   );
 };
