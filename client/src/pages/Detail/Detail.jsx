14211f36db5a1d02eccecf9adf1c81028d64c711
Lafsitoo <fretteleandro@live.com>
2023-02-10 20:27:05 -0300

    10/02-20:27

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -1,5 +1,18 @@
-import "./Detail.css";
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Detail = () => {};
+const Detail = ({ match }) => {
+  const dogs = useSelector((state) => state.dogs);
+  const dog = dogs.find((dog) => dog.id === match.params.id);
+
+  return (
+    <div className="dog-details">
+      <h2>{dog.name}</h2>
+      <p>Age: {dog.age}</p>
+      <p>Weight: {dog.weight}</p>
+      <p>Temperaments: {dog.temperament}</p>
+    </div>
+  );
+};
 
 export { Detail };
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import {Create} from "./pages/Create/Create"
 import { Landing } from "./pages/Landing/Landing";
+import { Detail } from "./pages/Detail/Detail";
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
         <Route index element={ <Landing /> } />
         <Route path={"/home"} element={<Home />} />
         <Route path={"/form"} element={<Create />} />
+        <Route path={"/dogs/:id"} element={<Detail />} />
         {/* <Route path={"/"} element={X} />
         <Route path={"/"} element={X} /> */}
       </Routes>
