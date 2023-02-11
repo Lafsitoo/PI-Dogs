75dcee375d37f6d6df52761e99a234f3e678db2e
Lafsitoo <fretteleandro@live.com>
2023-02-11 04:09:00 -0300

    11/02-04:09

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -1,30 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetail, getClean } from "./../../redux/actions/index";
-import "./Detail.css"
+import "./Detail.css";
 
 const Detail = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const dog = useSelector((state) => state.detail);
-  const [charged, setCharged] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
-      setCharged(false);
-    }, 600);
     dispatch(getDetail(id));
-    return () => {
-      getClean();
-    };
+    dispatch(getClean())
   }, [dispatch, id]);
 
   return (
-    <div >
-      {charged ? (
-        <></>
-      ) : (
+    <div>
+      {dog[0] ? (
         <div className="dog-details">
           <h2>{dog[0].name}</h2>
           <img src={dog[0].image} alt="" />
@@ -32,6 +24,10 @@ const Detail = () => {
           <p>Weight: {dog[0].weight}</p>
           <p>Heigth: {dog[0].height}</p>
           <p>Temperaments: {dog[0].temperament}</p>
+        </div>
+      ) : (
+        <div>
+          <h2>Loading...</h2>
         </div>
       )}
     </div>
